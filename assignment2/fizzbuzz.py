d0129de719cfb4079ade43972a7a6d929fd2058b
ShihaoDong1 <shihao.dong1@gmail.com>
2021-01-17 17:47:41 -0500

    completed fizzbuzz

---
+++ assignment2/fizzbuzz.py
@@ -0,0 +1,18 @@
+def fizzbuzz():
+    num = 1
+    while(num <= 100):
+        if((num % 3 == 0) and (num % 5 == 0)):
+            print(num, "FizzBuzz")
+        elif(num % 3 == 0):
+            print(num, "Fizz")
+        elif(num % 5 == 0):
+            print(num,  "Buzz")
+        else:
+            print(num)
+        num += 1
+
+
+def main():
+    fizzbuzz()
+
+main()
---
+++ assignment2/prime.py
@@ -0,0 +1,13 @@
+def prime(num):
+    ls = []
+    for x in range(1, num+1):
+        print(x)
+
+
+def main():
+    num = int(input("Enter a number: "))
+    print(num)
+    prime(num)
+
+
+main()
