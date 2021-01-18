6af8997ee0f2c59c0c63bcf4161b903dd0317379
ShihaoDong1 <shihao.dong1@gmail.com>
2021-01-18 02:38:18 -0500

    completed assignment 2

--- assignment2/my_sort.py
+++ assignment2/my_sort.py
@@ -1,8 +1,10 @@
 def my_sort(my_list):
-    for x in my_list:
-        print(my_list[0])
+    for i in range(len(my_list)):
+        for j in range(len(my_list) - 1):
+            if my_list[j] > my_list[j+1]:
+                my_list[j], my_list[j+1] = my_list[j+1], my_list[j]
 
 
-my_list = [2, 3, 4, 1, 5]
-
+my_list = [3, 1, 2]
 my_sort(my_list)
+print(my_list)
--- assignment2/unscramble.py
+++ assignment2/unscramble.py
@@ -1,15 +1,27 @@
 def unscramble():
     word = input("Enter word to find: ")
-    print(word)
-
+    ls = permutations(word)
+    new_ls = []
     f = open("wordlist.txt", "r")
-    lines = f.readlines()
+    content = f.read().splitlines()
+
+    for line in content:
+        if (len(line) == len(word)):
+            for x in range(len(ls)):
+                if ls[x] == line:
+                    new_ls.append(line)
+    print(new_ls)
 
 
-
-
-# for line in lines:
-#     print(line)
+def permutations(string):
+    permutation_list = []
+    if len(string) == 1:
+        return [string]
+    else:
+        for char in string:
+            [permutation_list.append(char + a)
+             for a in permutations(string.replace(char, "", 1))]
+    return permutation_list
 
 
 unscramble()
